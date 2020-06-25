956a44b3a9f53ec0ee6f1641499794012d597612
Hirota <hirotake.dadada@gmail.com>
2020-06-26 01:23:06 +0900

    Use function component.(end of chapter7)

--- hello-world/src/CharacterList.tsx
+++ hello-world/src/CharacterList.tsx
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+// FCはReactが用意している関数コンポーネント用の型
+// Function Componentの略
+import React, { FC } from "react";
 import { Header, Icon, Item } from "semantic-ui-react";
 
 export interface Character {
@@ -13,38 +15,32 @@ interface CharacterListProps {
   characters: Character[]
 }
 
-// ジェネリクス、型引数の<CharacterListProps>でpropsの型を指定している
-class CharacterList extends Component<CharacterListProps> {
-  render() {
-    // 自身のpropsをローカル変数として保持する
-    const { school, characters } = this.props
-
-    return (
-      // この謎の記号はReact.fragmentのシンタックスシュガー
-      // returnはひとつの要素という決まりがあるため、複数要素がネストする場合divタグで囲うことになる
-      // React.fragmentを使用すると、空divを書かなくても複数の要素を返却できる
-      <>
-        <Header as="h2">{school}</Header>
-        <Item.Group>
-          {characters.map(c => (
-            // ループ処理をする歳、要素に一意なkeyを設定しないとWarningが出る
-            // (仮想DOMから実DOM反映する際に使用されるので、ないとパフォーマンスも下がる）
-            <Item key={c.id}>
-              <Icon name="user circle" size="huge" />
-              <Item.Content>
-                <Item.Header>{c.name}</Item.Header>
-                <Item.Meta>{c.age}歳</Item.Meta>
-                <Item.Meta>
-                  {c.height ? c.height : '???'}
-                  cm
-                </Item.Meta>
-              </Item.Content>
-            </Item>
-          ))}
-        </Item.Group>
-      </>
-    );
-  }
-}
+// クラスではなく関数コンポーネントに変更
+const CharacterList: FC<CharacterListProps> = ({
+  // 引数がfalseの場合のデフォルト値
+  school = '校名不明',
+  characters,
+}) => (
+  <>
+    <Header as="h2">{school}</Header>
+    <Item.Group>
+      {characters.map(c => (
+        // ループ処理をする歳、要素に一意なkeyを設定しないとWarningが出る
+        // (仮想DOMから実DOM反映する際に使用されるので、ないとパフォーマンスも下がる）
+        <Item key={c.id}>
+          <Icon name="user circle" size="huge" />
+          <Item.Content>
+            <Item.Header>{c.name}</Item.Header>
+            <Item.Meta>{c.age}歳</Item.Meta>
+            <Item.Meta>
+              {c.height ? c.height : '???'}
+              cm
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+      ))}
+    </Item.Group>
+  </>
+);
 
 export default CharacterList;
