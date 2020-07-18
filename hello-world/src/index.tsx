830384355a9c461fd562a07f87aaccf19ec3bee0
Hirota <hirotake.dadada@gmail.com>
2020-07-18 23:03:10 +0900

    Use react router.(end of chapter9)

--- hello-world/src/index.tsx
+++ hello-world/src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './styles/semantic.min.css'
+import { BrowserRouter } from "react-router-dom";
+
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import App from './components/App';
-import Counter from './components/Counter';
-import TimerContainer from './containers/Timer';
+import './index.css';
+import './styles/semantic.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Counter />
-    <TimerContainer />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
